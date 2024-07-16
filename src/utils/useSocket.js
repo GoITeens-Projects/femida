e2fcc8af061eb632ab10940b12a41195c9a45e6d
m1xture <misha.ivanov.1050@gmail.com>
2024-07-16 21:30:34 +0300

    webscoket fix

--- src/utils/useSocket.js
+++ src/utils/useSocket.js
@@ -1,11 +1,18 @@
+const { clear } = require("../constants/newMap.js");
 const main = require("../index.js");
 const WebSocket = require("ws");
 
 module.exports = function useSocket(obj, msg) {
-  const ws = new WebSocket(process.env.FEMIDA_API);
+  const ws = new WebSocket("wss://" + process.env.FEMIDA_API + "/verify");
+  const timerId = setTimeout(async () => {
+    ws.close();
+    await msg.reply("Час вийшов, посилання для верифікації більше неактивне");
+  }, 1000 * 60 * 15);
   ws.onopen = (evt) => {
     ws.send(JSON.stringify(obj));
-    msg.channel.send("Пречудово. Відтепер у тебе є 15 хвилин на те, щоб перейти по посиланню в електронному листі")
+    // msg.channel.send(
+    //   "Пречудово. Відтепер у тебе є 15 хвилин на те, щоб перейти по посиланню в електронному листі"
+    // );
   };
   ws.onmessage = async ({ data }) => {
     const resp = JSON.parse(data);
@@ -13,20 +20,17 @@ module.exports = function useSocket(obj, msg) {
       msg.react("🚩");
     }
     if (resp.user) {
+      if (resp.user.discordId !== obj.id) return;
       const guild = main.client.guilds.cache.get("1192065857363394621");
       const member = await guild.members.fetch(resp.user.discordId);
       const role = guild.roles.cache.get("1262374645521190922");
-      console.log(member, "member");
-      console.log(role, "role");
+      // console.log(member, "member");
+      // console.log(role, "role");
       member.user.send("Перевір свої ролі на сервері))");
-      return member.roles.add(role);
+      member.roles.add(role);
+      clearTimeout(timerId);
+      return ws.close();
     }
     await msg.reply(resp.message);
   };
-  setTimeout(async () => {
-    ws.close();
-    await msg.followUp(
-      "Час вийшов, посилання для верифікації більше неактивне"
-    );
-  }, 1000 * 60 * 15);
 };
