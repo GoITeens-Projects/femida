a69f60e31cfb458f5c012bf602c242c5c7edf349
Oleksandr1shkin <cmurtimur@gmail.com>
2024-07-24 21:55:44 +0300

    Update editXP.js

--- src/commands/slashCommands/editXP.js
+++ src/commands/slashCommands/editXP.js
@@ -3,114 +3,111 @@ const Level = require("../../models/Level");
 const updateLevel = require("../../utils/updateLevel");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("edit-xp")
-    .setDescription("Додати чи відняти XP користувачу")
-    .addUserOption((option) =>
-      option
-        .setName("target-user")
-        .setDescription("Користувач XP якого ви хочете редагувати")
-        .setRequired(true)
-    )
-    .addNumberOption((option) =>
-      option
-        .setName("xp")
-        .setDescription("Скільки XP ви хочете додати або відняти користувачеві")
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("mode")
-        .setDescription(
-          "Виберіть «додати», щоб додати XP, або «відняти», щоб відняти XP"
+    data: new SlashCommandBuilder()
+        .setName("edit-xp")
+        .setDescription("Додати чи відняти XP користувачу")
+        .addUserOption((option) =>
+            option
+            .setName("target-user")
+            .setDescription("Користувач XP якого ви хочете редагувати")
+            .setRequired(true)
         )
-        .addChoices(
-          { name: "Add", value: "add" },
-          { name: "Subtract", value: "subtract" }
+        .addNumberOption((option) =>
+            option
+            .setName("xp")
+            .setDescription("Скільки XP ви хочете додати або відняти користувачеві")
+            .setRequired(true)
         )
-        .setRequired(true)
-    ),
+        .addStringOption((option) =>
+            option
+            .setName("mode")
+            .setDescription(
+                "Виберіть «додати», щоб додати XP, або «відняти», щоб відняти XP"
+            )
+            .addChoices({ name: "Add", value: "add" }, { name: "Subtract", value: "subtract" })
+            .setRequired(true)
+        ),
 
-  async execute(interaction) {
-    if (!interaction.inGuild()) {
-      await interaction.reply("Ви не можете запустити цю команду на сервері");
-      return;
-    }
+    async execute(interaction) {
+        if (!interaction.inGuild()) {
+            await interaction.reply("Ви не можете запустити цю команду на сервері");
+            return;
+        }
 
-    const allowedRoles = ["953717386224226385", "953795856308510760"];
+        const allowedRoles = ["953717386224226385", "953795856308510760", "1192066790717661245"];
 
-    const member = interaction.guild.members.cache.get(interaction.user.id);
+        const member = interaction.guild.members.cache.get(interaction.user.id);
 
-    const hasAllowedRole = member.roles.cache.some((role) =>
-      allowedRoles.includes(role.id)
-    );
+        const hasAllowedRole = member.roles.cache.some((role) =>
+            allowedRoles.includes(role.id)
+        );
 
-    if (!hasAllowedRole) {
-      interaction.reply(
-        "Тільки адміністрація має право використовувати цю команду🙃"
-      );
-      return;
-    }
+        if (!hasAllowedRole) {
+            interaction.reply(
+                "Тільки адміністрація має право використовувати цю команду🙃"
+            );
+            return;
+        }
 
-    await interaction.deferReply();
+        await interaction.deferReply();
 
-    const mentionedUserId = interaction.options.get("target-user")?.value;
-    const targetUserId = mentionedUserId || interaction.member.id;
-    const targetUserObj = await interaction.guild.members.fetch(targetUserId);
+        const mentionedUserId = interaction.options.get("target-user")?.value;
+        const targetUserId = mentionedUserId || interaction.member.id;
+        const targetUserObj = await interaction.guild.members.fetch(targetUserId);
 
-    let userInfo = await Level.findOne({
-      userId: targetUserId,
-      guildId: interaction.guild.id,
-    });
+        let userInfo = await Level.findOne({
+            userId: targetUserId,
+            guildId: interaction.guild.id,
+        });
 
-    if (!userInfo) {
-      const newUser = new Level({
-        userId: targetUserId,
-        guildId: interaction.guild.id,
-        xp: 0,
-        currentXp: 0,
-        level: 0,
-      });
+        if (!userInfo) {
+            const newUser = new Level({
+                userId: targetUserId,
+                guildId: interaction.guild.id,
+                xp: 0,
+                currentXp: 0,
+                level: 0,
+            });
 
-      await newUser.save();
-      userInfo = await Level.findOne({
-        userId: targetUserId,
-        guildId: interaction.guild.id,
-      });
-    }
-    const replyEmbed = new EmbedBuilder()
-      .setTitle("Операція прошла успішно")
-      .setColor("#FFD23F")
-      .setTimestamp();
-    if (interaction.options.get("mode").value === "add") {
-      const xpToAdd = interaction.options.get("xp").value;
-      userInfo.xp += xpToAdd;
-      await userInfo.save();
-      await updateLevel(userInfo, userInfo.userId);
-      const userNewXP = await Level.findOne({ userId: targetUserId });
-      await interaction.editReply({
-        embeds: [
-          replyEmbed.setDescription(
-            `\`${xpToAdd}\` XP було додано до ${targetUserObj.user.tag}. Новий XP користувача - \`${userNewXP.xp}\`. `
-          ),
-        ],
-      });
-    } else if (interaction.options.get("mode").value === "subtract") {
-      let xpToSubtract = interaction.options.get("xp").value;
-      if (xpToSubtract > userInfo.xp) {
-        xpToSubtract = userInfo.xp;
-      }
-      userInfo.xp -= xpToSubtract;
-      await userInfo.save();
-      await updateLevel(userInfo, userInfo.userId);
-      const userNewXP = await Level.findOne({ userId: targetUserId });
-      await interaction.editReply({
-        embeds: [
-          replyEmbed.setDescription(
-            `\`${xpToSubtract}\` XP було віднято у ${targetUserObj.user.tag}. Новий XP користувача - \`${userNewXP.xp}\`. `
-          ),
-        ],
-      });
-    }
-  },
+            await newUser.save();
+            userInfo = await Level.findOne({
+                userId: targetUserId,
+                guildId: interaction.guild.id,
+            });
+        }
+        const replyEmbed = new EmbedBuilder()
+            .setTitle("Операція прошла успішно")
+            .setColor("#FFD23F")
+            .setTimestamp();
+        if (interaction.options.get("mode").value === "add") {
+            const xpToAdd = interaction.options.get("xp").value;
+            userInfo.xp += xpToAdd;
+            await userInfo.save();
+            await updateLevel(userInfo, userInfo.userId);
+            const userNewXP = await Level.findOne({ userId: targetUserId });
+            await interaction.editReply({
+                embeds: [
+                    replyEmbed.setDescription(
+                        `\`${xpToAdd}\` XP було додано до ${targetUserObj.user.tag}. Новий XP користувача - \`${userNewXP.xp}\`. `
+                    ),
+                ],
+            });
+        } else if (interaction.options.get("mode").value === "subtract") {
+            let xpToSubtract = interaction.options.get("xp").value;
+            if (xpToSubtract > userInfo.xp) {
+                xpToSubtract = userInfo.xp;
+            }
+            userInfo.xp -= xpToSubtract;
+            await userInfo.save();
+            await updateLevel(userInfo, userInfo.userId);
+            const userNewXP = await Level.findOne({ userId: targetUserId });
+            await interaction.editReply({
+                embeds: [
+                    replyEmbed.setDescription(
+                        `\`${xpToSubtract}\` XP було віднято у ${targetUserObj.user.tag}. Новий XP користувача - \`${userNewXP.xp}\`. `
+                    ),
+                ],
+            });
+        }
+    },
 };
