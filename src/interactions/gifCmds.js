ae7e291377c2229305ec287c2ab2c424d886afc3
m1xture <misha.ivanov.1050@gmail.com>
2024-07-25 19:26:59 +0300

    actions cmds: cooldown added

--- src/interactions/gifCmds.js
+++ src/interactions/gifCmds.js
@@ -3,13 +3,38 @@ const {
   EmbedBuilder,
   AttachmentBuilder,
 } = require("discord.js");
-require("dotenv").config();
-const { Kawaii } = require("kawaii-api");
-const api = new Kawaii(process.env.KAWAII_TOKEN);
 const getColors = require("get-image-colors");
+const gifs = require("discord-actions");
+const NodeCache = require("node-cache");
+const usersInCooldown = new NodeCache({ stdTTL: 30 });
 
 module.exports = async function gifCmds(interaction, actionText) {
+  async function antiSpamCmds() {
+    const timeLeft = Math.round(
+      (usersInCooldown.get(interaction.user.id).getTime() -
+        new Date().getTime()) /
+        1000
+    );
+    if (timeLeft < 0.5) {
+      return null;
+    }
+    return await interaction.reply({
+      content: `Не використовуй команди реакцій настільки швидко. Зачекай ще ${timeLeft} секунд${
+        ((timeLeft.toString().endsWith("2") ||
+          timeLeft.toString().endsWith("3") ||
+          timeLeft.toString().endsWith("4")) &&
+          timeLeft > 15) ||
+        timeLeft < 5
+          ? "и"
+          : ""
+      }${timeLeft.toString().endsWith(1) ? "у" : ""}`,
+      ephemeral: true,
+    });
+  }
   try {
+    if (usersInCooldown.get(interaction.user.id)) {
+      if (await antiSpamCmds()) return;
+    }
     await interaction.deferReply();
     const mentionedUserId = interaction.options.get("target-user")?.value;
     const targetUserObj = await interaction.guild.members.fetch(
@@ -23,8 +48,8 @@ module.exports = async function gifCmds(interaction, actionText) {
       await interaction.editReply("Не знущайся над ботами. Ми хороші)");
       return;
     }
-    const gifUrl = await api.get("gif", interaction.commandName);
-    const colors = await getColors(gifUrl);
+    const { url } = await gifs[interaction.commandName]();
+    const colors = await getColors(url);
     const embed = new EmbedBuilder()
       .setTitle(
         `${
@@ -42,8 +67,10 @@ module.exports = async function gifCmds(interaction, actionText) {
         }`
       )
       .setColor(colors[0]._rgb)
-      .setImage(gifUrl);
+      .setImage(url);
     await interaction.editReply({ embeds: [embed] });
+    const expiresAfter = new Date(new Date().getTime() + 1000 * 30);
+    usersInCooldown.set(interaction.user.id, expiresAfter);
   } catch (err) {
     console.log(err);
   }
