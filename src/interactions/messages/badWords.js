59b477365016345c6e3530d6c2626b9f9c73515e
m1xture <misha.ivanov.1050@gmail.com>
2025-02-02 19:15:43 +0200

    verification message fix + badwords settings added

--- src/interactions/messages/badWords.js
+++ src/interactions/messages/badWords.js
@@ -5,47 +5,77 @@ const {
 const { giveWarn, giveSoftWarn } = require("../../utils/warnSystem");
 const SettingsInterface = require("../../utils/settings");
 const Level = require("../../models/Level");
+const addNewMember = require("../addNewMember");
+
+async function firstWarn(message, settings) {
+  if (settings.badwords.actions.giveWarn)
+    await giveSoftWarn(
+      message.author.id,
+      "Використання нецензурної лексики (вперше)"
+    );
+  if (settings.badwords.actions.notifyUser.enabled)
+    message.channel.send(
+      `<@${message.author.id}> не використовуй нецензурну лексику! \n Так як у тебе це вперше, ти отримав(-ла) "м'яке" попередження`
+    );
+  if (settings.badwords.actions.deleteMsg) message.delete();
+}
+
+async function anotherWarn(message, settings) {
+  if (settings.badwords.actions.giveWarn)
+    await giveWarn(
+      message.author.id,
+      "Неодноразове використання нецензурної лексики"
+    );
+  if (settings.badwords.actions.notifyUser.enabled)
+    message.channel.send(
+      `<@${message.author.id}> не використовуй нецензурну лексику! Додаю тобі твій черговий варн`
+    );
+  if (settings.badwords.actions.deleteMsg) await message.delete();
+  if (settings.badwords.actions.mute.enabled)
+    await message.member.timeout(
+      settings.mute.muteTimeMs ? settings.mute.muteTimeMs : 1000 * 60 * 30,
+      "Не перше використання нецензурної лайки"
+    );
+}
 
 module.exports = async (message) => {
   try {
-    if (message.member.roles.cache.some((role) => adminRoles.includes(role.id)))
+    const settings = await SettingsInterface.getSettings();
+    if (!settings.badwords.enabled) return;
+
+    if (
+      message.member.roles.cache.some((role) => adminRoles.includes(role.id)) &&
+      settings.badwords.actions.ignoreAdmins
+    )
       return;
 
-    const settings = await SettingsInterface.getSettings();
-    // console.log('bad', settings.badwords.words);
     for (const word of settings.badwords.words) {
       if (!message.content.toLowerCase().includes(word.toLowerCase())) continue;
-      const userObj = await Level.findOne({ userId: message.author.id });
-      if (userObj) {
-        if (userObj.warnings?.amount > 0) {
-          await giveWarn(
-            message.author.id,
-            "Неодноразове використання нецензурної лексики"
-          );
-          message.channel.send(
-            `<@${message.author.id}> не використовуй нецензурну лексику! Додаю тобі твій черговий варн`
-          );
-          await message.delete();
-          await message.member.timeout(
-            settings.mute.muteTimeMs
-              ? settings.mute.muteTimeMs
-              : 1000 * 60 * 30,
-            "Не перше використання нецензурної лайки"
-          );
-          break;
-        } else {
-          await giveSoftWarn(
-            message.author.id,
-            "Використання нецензурної лексики (вперше)"
-          );
-          message.channel.send(
-            `<@${message.author.id}> не використовуй нецензурну лексику! \n Так як у тебе це вперше, ти отримав(-ла) "м'яке" попередження`
-          );
-          message.delete();
-          break;
-        }
+      let userObj = await Level.findOne({ userId: message.author.id });
+      if (!userObj) {
+        addNewMember(null, message);
+        userObj = await Level.findOne({ userId: message.author.id });
       }
-      // const mutedRole = guild.roles.cache.get(mutedRoleID);
+      if (!settings.badwords.actions.giveWarn) break;
+      if (userObj.warnings?.amount > 0) {
+        // await giveWarn(
+        //   message.author.id,
+        //   "Неодноразове використання нецензурної лексики"
+        // );
+        // message.channel.send(
+        //   `<@${message.author.id}> не використовуй нецензурну лексику! Додаю тобі твій черговий варн`
+        // );
+        // await message.delete();
+        // await message.member.timeout(
+        //   settings.mute.muteTimeMs ? settings.mute.muteTimeMs : 1000 * 60 * 30,
+        //   "Не перше використання нецензурної лайки"
+        // );
+        await anotherWarn(message);
+        break;
+      } else {
+        await firstWarn(message);
+        break;
+      }
     }
   } catch (error) {
     console.error("Error processing message:", error);
