a9ac41636c9aa43328ec8771123116285ec12bd7
Oleksandr1shkin <cmurtimur@gmail.com>
2025-03-12 15:45:50 +0200

    почав роботу з підключенням до бд

--- src/constants/antiSpam.js
+++ src/constants/antiSpam.js
@@ -1,9 +1,19 @@
-module.exports = {
-    warnThreshold: 3,
-    muteTreshold: 4,
-    warnMessage: "Припини спамити!:warning:",
-    muteMessage: "Ти отримав мут за спам!:warning:",
-    unMuteTime: 60,
-    verbose: true,
-    removeMessages: true,
-};
+const SettingsInterface = require("../../utils/settings");
+
+async function getSpamSettings() {
+    const settings = await SettingsInterface.getSettings();
+  return settings.spam?.messagesLimit || 4; // Якщо в БД нема значення, то дефолтне 4
+}
+
+module.exports = (async() => {
+    const muteTreshold = await getSpamSettings();
+    return {
+        warnThreshold: 3,
+        muteTreshold,
+        warnMessage: "Припини спамити!:warning:",
+        muteMessage: "Ти отримав мут за спам!:warning:",
+        unMuteTime: 60,
+        verbose: true,
+        removeMessages: true,
+    };
+})();
--- src/interactions/messages/useAntispam.js
+++ src/interactions/messages/useAntispam.js
@@ -1,72 +1,74 @@
 const NodeCache = require("node-cache");
 const antiSpam = require("../../constants/antiSpam.js");
+const SettingsInterface = require("../../utils/settings");
 const {
-  roles: { adminRoles },
+    roles: { adminRoles },
 } = require("../../constants/config.js");
 const WarnSystem = require("../../utils/warnSystem.js");
 const myCache = new NodeCache({
-  stdTTL: 30, // seconds
+    stdTTL: 30, // seconds
 });
 
-module.exports = async (message) => {
-  const userId = message.author.id;
-  const content = message.content;
+module.exports = async(message) => {
+    const userId = message.author.id;
+    const content = message.content;
 
-  try {
-    const member = message.guild.members.cache.get(userId);
-    const hasAdminRoles = adminRoles.some((role) =>
-      member.roles.cache.has(role)
-    );
 
-    if (hasAdminRoles) return;
-
-    const cacheKey = `${userId}_${content}`;
-
-    const cachedMessages = myCache.get(cacheKey) || [];
-
-    cachedMessages.push({
-      messageId: message.id,
-      channelId: message.channel.id,
-    });
-
-    myCache.set(cacheKey, cachedMessages);
-    console.log("Cached messages length:", cachedMessages.length);
-
-    if (cachedMessages.length >= antiSpam.warnThreshold) {
-      if (cachedMessages.length >= antiSpam.muteTreshold) {
-        await message.guild.members.cache
-          .get(userId)
-          .timeout(1000 * 60 * 60 * 2, "Мут за спам");
-        await WarnSystem.giveWarn(
-          userId,
-          "Спрацювання системи антиспаму",
-          true
+    try {
+        const member = message.guild.members.cache.get(userId);
+        const hasAdminRoles = adminRoles.some((role) =>
+            member.roles.cache.has(role)
         );
-        await message.channel.send(`<@${userId}> ${antiSpam.muteMessage}`);
-      } else {
-        await message.channel.send(`<@${userId}> ${antiSpam.warnMessage}`);
-        await WarnSystem.giveSoftWarn(userId, "Мут за спам", false);
-        for (const cachedMsg of cachedMessages) {
-          try {
-            const channel = message.guild.channels.cache.get(
-              cachedMsg.channelId
-            );
-            if (!channel) continue;
 
-            const spamMessage = await channel.messages
-              .fetch(cachedMsg.messageId)
-              .catch(() => null);
-            if (!spamMessage) continue;
+        if (hasAdminRoles) return;
 
-            await spamMessage.delete();
-          } catch (error) {
-            console.error("Error deleting message:", error);
-          }
+        const cacheKey = `${userId}_${content}`;
+
+        const cachedMessages = myCache.get(cacheKey) || [];
+
+        cachedMessages.push({
+            messageId: message.id,
+            channelId: message.channel.id,
+        });
+
+        myCache.set(cacheKey, cachedMessages);
+        console.log("Cached messages length:", cachedMessages.length);
+
+        if (cachedMessages.length >= antiSpam.warnThreshold) {
+            if (cachedMessages.length >= antiSpam.muteTreshold) {
+                await message.guild.members.cache
+                    .get(userId)
+                    .timeout(1000 * 60 * 1, "Мут за спам");
+                await WarnSystem.giveWarn(
+                    userId,
+                    "Спрацювання системи антиспаму",
+                    true
+                );
+                await message.channel.send(`<@${userId}> ${antiSpam.muteMessage}`);
+            } else {
+                await message.channel.send(`<@${userId}> ${antiSpam.warnMessage}`);
+                await WarnSystem.giveSoftWarn(userId, "Мут за спам", false);
+                for (const cachedMsg of cachedMessages) {
+                    try {
+                        const channel = message.guild.channels.cache.get(
+                            cachedMsg.channelId
+                        );
+                        if (!channel) continue;
+
+                        const spamMessage = await channel.messages
+                            .fetch(cachedMsg.messageId)
+                            .catch(() => null);
+                        if (!spamMessage) continue;
+
+                        await spamMessage.delete();
+                    } catch (error) {
+                        console.error("Error deleting message:", error);
+                    }
+                }
+                return;
+            }
         }
-        return;
-      }
+    } catch (err) {
+        console.error("antispamErr:", err);
     }
-  } catch (err) {
-    console.error("antispamErr:", err);
-  }
 };
