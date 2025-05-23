502e8a9a6266393b61515aeff2352ffd2c157d8a
Oleksandr1shkin <105589389+Oleksandrishkin@users.noreply.github.com>
2025-05-23 15:51:06 +0300

    пофіксив баг

--- src/utils/xp/updateLevel.js
+++ src/utils/xp/updateLevel.js
@@ -3,9 +3,12 @@ const sendLevelNotification = require("./sendLevelNotification");
 const sendDmMsg = require("./sendDmMsg");
 const addRoleLevel = require("./addRoleLevel");
 const addGifters = require("./addGifters");
+const cfg = require("../../constants/config");
 
 module.exports = async function updateLevel({ level, xp }, userId) {
   let newLevel = 0;
+
+  // Функція для обчислення потрібного XP для рівня
   function calculateXPForLevel(lvl) {
     let xpForLevel = 0;
     for (let i = 0; i < lvl; i++) {
@@ -13,34 +16,35 @@ module.exports = async function updateLevel({ level, xp }, userId) {
     }
     return xpForLevel;
   }
+
+  // Обчислюємо новий рівень
   while (xp >= calculateXPForLevel(newLevel + 1)) {
     newLevel++;
   }
+
   if (newLevel > level) {
     const params = { id: userId, level: newLevel };
     await sendLevelNotification(params);
-    if (newLevel - level >= 2) {
-      for (let i = level + 1; i <= newLevel; i++) {
-        if (i % 5 !== 0) continue;
-        try {
+
+    // Рівні, на які треба видавати ролі
+    const roleLevels = cfg.levelRoles.map(role => role.level);
+
+    for (let i = level + 1; i <= newLevel; i++) {
+      try {
+        if (roleLevels.includes(i)) {
           await sendDmMsg({ id: userId, level: i });
           await addRoleLevel({ level: i, xp }, userId);
           // await addGifters(userId, i);
-        } catch (err) {
-          console.log("Level Error 2 - " + err);
         }
-      }
-    } else if (newLevel % 5 === 0 && newLevel !== 0) {
-      try {
-        // await sendDmMsg(params);
-        await addRoleLevel({ level: newLevel, xp }, userId);
-        // await addGifters(userId, newLevel);
       } catch (err) {
         console.log("Level Error - " + err);
       }
     }
   }
+
   if (level === newLevel) return level;
+
   await Level.findOneAndUpdate({ userId }, { level: newLevel });
+
   return newLevel;
 };
