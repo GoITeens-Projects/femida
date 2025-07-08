3e90fa02b02a9066bb2f504fcc6a07e7524c089a
m1xture <misha.ivanov.1050@gmail.com>
2025-07-08 14:02:27 +0300

    exit message fix

--- src/utils/sendExitMessage.js
+++ src/utils/sendExitMessage.js
@@ -2,7 +2,7 @@ const {
   channels: { emergencyChannel },
 } = require("../constants/config");
 
-const sendExitMessage = async () => {
+const sendExitMessage = async (client) => {
   const channel = await client.channels.fetch(emergencyChannel);
   await channel.send(
     "<@579623837495328808> <@1137391988417769583> я впала🥀 \nпідійміть мене будь ласка"
