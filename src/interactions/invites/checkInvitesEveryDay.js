8f9136662ae0b636b6ac86abdfaec65522ac1808
m1xture <misha.ivanov.1050@gmail.com>
2025-08-29 18:16:31 +0300

    invites fix

--- src/interactions/invites/checkInvitesEveryDay.js
+++ src/interactions/invites/checkInvitesEveryDay.js
@@ -3,7 +3,7 @@ const InviteSystem = require("./invitesSystem");
 
 module.exports = async () => {
   const invitesCheckJob = new cron.CronJob(
-    "00 46 21 * * *",
+    "00 35 14 * * *",
     InviteSystem.dailyCheckInvites,
     null,
     true,
